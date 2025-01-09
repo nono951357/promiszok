ff73a1cb58a8769a4ceff7a7cdf9c0e0af933e32
nono951357 <berkileventemarko@gmail.com>
2025-01-09 10:53:16 +0100

    added build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/promiszok/',
+    build: {
+        outDir: 'docs',
+        emptyOutDir: true
+    }
 })
